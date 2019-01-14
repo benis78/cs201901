004983d1e5aecd2faf645744676500e56b393978
Jesper <admin@TA-829-06>
2019-01-14 15:52:39 +0100

    Ny kode

--- opgave_variabeltal/Program.cs
+++ opgave_variabeltal/Program.cs
@@ -10,6 +10,39 @@ namespace opgave_variabeltal
     {
         static void Main(string[] args)
         {
+            int heltal = 10;
+            heltal++;
+            heltal--;
+            heltal += 20;
+
+            Console.WriteLine(heltal);
+
+            double kommatal =12.5;
+            kommatal++;
+            kommatal--;
+            kommatal*= 2;
+            
+
+            Console.WriteLine($"kommatal = {kommatal}");
+       
+            
+
+
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+
+
+
+
+
         }
+
+
     }
 }
