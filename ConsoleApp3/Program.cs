2b7a6662b9ba7bd73ceebc1ce1def4dbe3242de2
Jesper <admin@TA-829-06>
2019-01-15 15:55:34 +0100

    sdf

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 99; i++)
+
+                
+
+                Console.WriteLine(i);    
+
+                if (System.Diagnostics.Debugger.IsAttached)
+                {
+                    Console.Write("Press any key to continue . . . ");
+                    Console.ReadKey();
+                }
+
+            
+
+        }
+    }
+}
