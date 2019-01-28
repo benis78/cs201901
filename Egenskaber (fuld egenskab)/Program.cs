d5ac73ee53d52157a2e635ed3d7e42baf143039a
admin <admin@TA-8214-08>
2019-01-28 11:32:30 +0100

    .

--- Egenskaber (fuld egenskab)/Program.cs
+++ Egenskaber (fuld egenskab)/Program.cs
@@ -10,6 +10,129 @@ namespace Egenskaber__fuld_egenskab_
     {
         static void Main(string[] args)
         {
+            Person p1 = new Person() { Fornavn = "jesper", Efternavn = "Hansen" };
+            Person p2 = new Person() { Fornavn = "Toke" };
+
+            Elev e1 = new Elev() { Fornavn = "Egon", Efternavn = "Nørd", Klasselokale=34};
+            Instruktør i1 = new Instruktør() { Fornavn = "Ivan", Efternavn = "Intens"};
+
+            int e2 =e1.Klasselokale;
+
+            Console.WriteLine(p1.FuldtNavn());
+            Console.WriteLine(p2.FuldtNavn());
+            Console.WriteLine(e1.FuldtNavn());
+            Console.WriteLine(e2);
+            Console.WriteLine(i1.FuldtNavn());
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+
+        class Person
+        {
+            public String Fornavn { get; set; }
+            public String Efternavn { get; set; }
+
+            public String FuldtNavn()
+            {
+                return $"{Fornavn} {Efternavn}";
+
+            }
+        }
+
+        class Elev : Person
+        {
+            public int Klasselokale { get; set; }
+
+        }
+
+        class Instruktør : Person
+        {
+            public int NøgleId { get; set; }
         }
     }
+    //class Program
+
+    //{
+
+    //    static void Main(string[] args)
+
+    //    {
+
+    //        Person p = new Person() { Efternavn = "x", Fornavn = "y" };
+
+    //        Elev e = new Elev() { Efternavn = "t", Fornavn = "g", KlasseLokale = "A" };
+
+    //        Instruktør i = new Instruktør() { Efternavn = "2", Fornavn = "s", NøgleId = 1 };
+
+    //        Console.WriteLine(p.FuldtNavn());
+
+    //        Console.WriteLine(e.FuldtNavn());
+
+    //        Console.WriteLine(i.FuldtNavn());
+
+    //        // Hold console åben ved debug
+
+    //        if (System.Diagnostics.Debugger.IsAttached)
+
+    //        {
+
+    //            Console.Write("Press any key to continue . . . ");
+
+    //            Console.ReadKey();
+
+    //        }
+
+    //    }
+
+    //}
+
+
+
+    //public class Person
+
+    //{
+
+    //    public string Fornavn { get; set; }
+
+    //    public string Efternavn { get; set; }
+
+    //    public string FuldtNavn()
+
+    //    {
+
+    //        return $"{Fornavn} {Efternavn}";
+
+    //    }
+
+
+
+    //}
+
+
+
+    //public class Elev : Person
+
+    //{
+
+    //    public string KlasseLokale { get; set; }
+
+    //}
+
+
+
+    //public class Instruktør : Person
+
+    //{
+
+    //    public int NøgleId { get; set; }
+
+    //}
+
+
 }
