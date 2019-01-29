5608fd6847a9f48794cd7510a2d5351512044378
admin <admin@TA-8214-08>
2019-01-29 10:54:30 +0100

    .

---
+++ Interface (compare)/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hund[] hunde = new Hund[5];
+            hunde[0] = new Hund() { Alder = 10, Navn = "Bulder" };
+            hunde[1] = new Hund() { Alder = 5, Navn = "snubbi" };
+            hunde[2] = new Hund() { Alder = 3, Navn = "klat" };
+            hunde[3] = new Hund() { Alder = 34, Navn = "Pøls" };
+            hunde[4] = new Hund() { Alder = 2, Navn = "freddi" };
+            Array.Sort(hunde);
+            foreach (var item in hunde)
+            {
+                Console.WriteLine(item.Navn +" "+ item.Alder);
+
+    
+            }
+
+            
+
+                if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    class Hund:IComparable<Hund>
+    {
+        public string Navn { get; set; }
+        public int Alder { get; set; }
+
+        public int CompareTo(Hund other)
+        {
+            return this.Alder.CompareTo(other.Alder);
+        }
+    }
+}
---
+++ Interface (compare)/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Interface (compare)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Interface (compare)")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1ac46dd6-d0ce-4280-9725-b71aba6bceba")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
